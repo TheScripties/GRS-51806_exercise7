ee2eed00dfe38ca0c66375e71225ff436e2633bb
unknown <winke023@D0117105.wurnet.nl>
2015-01-15 21:51:19 +0100

    Just some minor additions, comments at the end of main and treeCover script.

--- R/treeCover.R
+++ R/treeCover.R
@@ -59,3 +59,21 @@ linearModel <- function(GewataBrick) {
   plot(GewataLM)
   return(GewataLM)
 }
+
+# function 
+#create training table with names same as raster layers in input brick
+
+#Done
+
+#(1)#produce one or more plots that demonstrate the relationship between the Landsat bands and the VCF tree cover. 
+###What can you conclude from this/these plot(s)?
+#(2)#create an lm() model and show a summary (e.g. using summary()) of the model object you created.
+
+#To Do
+#(2)
+###Which predictors (bands) are probably most important in predicting tree cover?
+#(3)#plot the predicted tree cover raster and compare with the original VCF raster.
+#(4)#compute the RMSE between your predicted and the actual tree cover values (hint )
+#(5)#are the differences between the predicted and actual tree cover the same for all of the 3 classes we used for 
+###the random forest classfication? Using the training polygons from the random forest classification, 
+###calculate the RMSE separately for each of the classes and compare. Hint - see ?zonal().
--- main.R
+++ main.R
@@ -22,4 +22,14 @@ treeCover()
     # All the plots indicate that a decrease in reflection results in an increase in VCF.
 # Question 2
   # Which predictors (bands) are probably most important in predicting tree cover?
+    # The bands that are probably most important are bands 
     #
+    #Coefficients:
+    #                 Estimate    Std.Error  t value    Pr(>|t|)    
+    #(Intercept)       8.549e+01  5.723e-02 1493.805   <2e-16 ***
+    #valuetable$band1  9.291e+02  1.889e+00  491.817   <2e-16 ***
+    #valuetable$band2 -1.505e+03  2.353e+00 -639.622   <2e-16 ***
+    #valuetable$band3 -2.528e+01  1.698e+00  -14.889   <2e-16 ***
+    #valuetable$band4  1.661e+02  2.790e-01  595.378   <2e-16 ***
+    #valuetable$band5 -2.006e+02  7.036e-01 -285.119   <2e-16 ***
+    #valuetable$band7  2.549e-01  8.969e-01    0.284    0.776    
